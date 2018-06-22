b458ae6f70b62d5550164ef7028015cd7138fc76
xianbu <dang.xia@qq.com>
2018-06-22 09:35:29 +0800

    perm fix

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -4,11 +4,13 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.utils import timezone
 from django.core.mail import send_mail
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User, Group, _user_get_all_permissions, _user_has_perm, _user_has_module_perms
+from django.contrib.auth.models import Permission as DjangoPermission
 from django.contrib.postgres.fields import JSONField
 from functools import reduce
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
 
 class StaffManager(BaseUserManager):
 
@@ -73,9 +75,84 @@ class Resource(models.Model):
     class Meta:
         verbose_name_plural = verbose_name = u'资源'
 
+class MyPermissionsMixin(models.Model):
+    """
+    Add the fields and methods necessary to support the Group and Permission
+    models using the ModelBackend.
+    """
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        help_text=_(
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.'
+        ),
+        related_name="user_set",
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        DjangoPermission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_name="user_set",
+        related_query_name="user",
+    )
 
-class Staff(AbstractBaseUser):
-    username = models.CharField(u"用户名", max_length=24, default="")
+    class Meta:
+        abstract = True
+
+    def get_group_permissions(self, obj=None):
+        """
+        Return a list of permission strings that this user has through their
+        groups. Query all available auth backends. If an object is passed in,
+        return only permissions matching this object.
+        """
+        permissions = set()
+        for backend in auth.get_backends():
+            if hasattr(backend, "get_group_permissions"):
+                permissions.update(backend.get_group_permissions(self, obj))
+        return permissions
+
+    def get_all_permissions(self, obj=None):
+        return _user_get_all_permissions(self, obj)
+
+    def has_perm(self, perm, obj=None):
+        """
+        Return True if the user has the specified permission. Query all
+        available auth backends, but return immediately if any backend returns
+        True. Thus, a user who has permission from a single auth backend is
+        assumed to have permission in general. If an object is provided, check
+        permissions for that object.
+        """
+        # Active superusers have all permissions.
+        if self.is_active and self.is_superuser:
+            return True
+
+        # Otherwise we need to check the backends.
+        return _user_has_perm(self, perm, obj)
+
+    def has_perms(self, perm_list, obj=None):
+        """
+        Return True if the user has each of the specified permissions. If
+        object is passed, check if the user has all required perms for it.
+        """
+        return all(self.has_perm(perm, obj) for perm in perm_list)
+
+    def has_module_perms(self, app_label):
+        """
+        Return True if the user has any permissions in the given app label.
+        Use simlar logic as has_perm(), above.
+        """
+        # Active superusers have all permissions.
+        if self.is_active and self.is_superuser:
+            return True
+
+        return _user_has_module_perms(self, app_label)
+
+class Staff(AbstractBaseUser, MyPermissionsMixin):
+    username = models.CharField(u"用户名", max_length=24, unique=True, db_index=True, default="")
     email = models.EmailField(u"邮件", max_length=255, unique=True, db_index=True)
     is_active = models.BooleanField("是否激活", default=False)
     is_superuser = models.BooleanField("是否是超级用户", default=False)
@@ -101,13 +178,6 @@ class Staff(AbstractBaseUser):
         if not menus:
             return []
         return list(set(reduce(lambda x, y: x + y, menus)))
-
-    def has_module_perms(self, demo):
-        return True
-
-
-    def has_perm(self, perm, obj=None):
-        return True
 
     def email_user(self, subject, message, from_email=None):
         """
@@ -139,6 +209,17 @@ class Staff(AbstractBaseUser):
     class Meta:
         verbose_name_plural = verbose_name = u'活动用户'
 
+class UserManagement(Staff):
+    class Meta:
+        proxy = True
+        verbose_name = '用户管理'
+        verbose_name_plural = '用户管理'
+
+class UserAuthentication(Staff):
+    class Meta:
+        proxy = True
+        verbose_name = '用户授权'
+        verbose_name_plural = '用户授权'
 
 @receiver(post_save, sender=Staff)
 def assign_default_jiaodui_role(sender, instance, **kwargs):
